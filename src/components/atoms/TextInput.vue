8961b60cc7d4c16de32136421441d4c77ae2ac41
Chun-igaw <Chun.lee@igaworks.com>
2022-09-28 18:16:04 +0900

    feat: #3 전화번호 등록 기능 구현 - 유효성 검사 - 유효성 검사 결과에 따른 메시지 분기 - 전화번호 "-" 자동 입력 - 폼 제출

---
+++ src/components/atoms/TextInput.vue
@@ -0,0 +1,64 @@
+<template>
+  <div class="container">
+    <!--속성값에 동적 데이터 넣고 싶으면 v-vind 해서 연결-->
+    <label :for="inputTitle">{{ inputTitle }}: </label>
+    <input
+      type="text"
+      :id="inputTitle"
+      :placeholder="placeholder"
+      :value="value.text"
+      :maxlength="maxLength"
+      @input="handleInput"
+    />
+    <p :class="hidden">{{ inputTitle }}을 입력해주세요</p>
+    <p :class="show" v-if="value.isValid">검사 통과</p>
+    <p :class="show" v-else>검사 통과X</p>
+  </div>
+</template>
+
+<script lang="ts">
+import Vue, { PropType } from "vue";
+import { InputValue } from "@/components/organisms/AddPhoneNumber.vue";
+
+export default Vue.extend({
+  props: {
+    inputTitle: String,
+    placeholder: String,
+    value: {
+      type: Object as PropType<InputValue>,
+      required: true,
+    },
+    maxLength: Number,
+  },
+
+  computed: {
+    hidden() {
+      return this.value.text.length > 0 ? "hidden" : null;
+    },
+    show() {
+      return this.value.text.length > 0 ? null : "hidden";
+    },
+  },
+
+  methods: {
+    handleInput(event: InputEvent) {
+      if (!event.target) {
+        return;
+      }
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit("input", eventTarget.value);
+    },
+  },
+});
+</script>
+
+<style scoped>
+.container {
+  margin: 10px 10px;
+  border: none;
+}
+
+.hidden {
+  display: none;
+}
+</style>
--- src/components/organisms/AddPhoneNumber.vue
+++ src/components/organisms/AddPhoneNumber.vue
@@ -1,11 +1,153 @@
 <template>
-  <div></div>
+  <div class="container">
+    <h3>전화번호 등록</h3>
+    <form @submit="onSubmitInfo">
+      <TextInput
+        inputTitle="이름"
+        :value="name"
+        placeholder="영어,공백 포함 20자 이하"
+        @input="updateName"
+      ></TextInput>
+      <TextInput
+        inputTitle="이메일"
+        :value="email"
+        @input="updateEmail"
+        placeholder="40자 이하"
+      ></TextInput>
+      <TextInput
+        inputTitle="전화번호"
+        placeholder="숫자만 입력"
+        :value="phoneNumber"
+        :maxLength="13"
+        @input="updatePhoneNumber"
+      ></TextInput>
+      <AtomicButton
+        button-name="전화번호 등록"
+        :is-passed="isPassed"
+      ></AtomicButton>
+    </form>
+  </div>
 </template>
 
-<script>
-export default {
-  name: "AddPhoneNumber",
-};
+<script lang="ts">
+import Vue from "vue";
+import TextInput from "@/components/atoms/TextInput.vue";
+import AtomicButton from "@/components/atoms/AtomicButton.vue";
+
+export interface InputValue {
+  text: string;
+  isValid: boolean;
+}
+export default Vue.extend({
+  components: { AtomicButton, TextInput },
+  data() {
+    return {
+      name: {
+        text: "",
+        isValid: false,
+      },
+      email: {
+        text: "",
+        isValid: false,
+      },
+      phoneNumber: {
+        text: "",
+        isValid: false,
+      },
+    };
+  },
+  computed: {
+    // 버튼 활성화 버튼
+    isPassed(): boolean {
+      let result =
+        this.name.isValid && this.email.isValid && this.phoneNumber.isValid;
+      return result;
+    },
+  },
+  methods: {
+    // data 변경 & 유효성 검사
+    updateName(value: string) {
+      this.validateName(value);
+      this.name.text = value;
+    },
+    updateEmail(value: string) {
+      this.validateEmail(value);
+      this.email.text = value;
+    },
+    updatePhoneNumber(value: string) {
+      this.validatePhoneNumber(value);
+      value = value.replace(/[^0-9]/g, "");
+      if (value.length < 3) {
+        this.phoneNumber.text = value;
+      } else {
+        if (value.substring(0, 2) === "02") {
+          if (value.length <= 5) {
+            this.phoneNumber.text =
+              value.substring(0, 2) + "-" + value.substring(2, 6);
+          } else if (value.length > 5 && value.length <= 9) {
+            this.phoneNumber.text =
+              value.substring(0, 2) +
+              "-" +
+              value.substring(2, 6) +
+              "-" +
+              value.substring(6, 11);
+          } else if (value.length > 9) {
+            this.phoneNumber.text =
+              value.substring(0, 2) +
+              "-" +
+              value.substring(2, 6) +
+              "-" +
+              value.substring(6, 11);
+          }
+        } else {
+          if (value.length < 8) {
+            this.phoneNumber.text = value;
+          } else if (value.length == 8) {
+            // console.log(value.substring(0, 3));
+            // console.log(value.substring(3, 8));
+            this.phoneNumber.text =
+              value.substring(0, 3) + "-" + value.substring(3, 8);
+          } else if (value.length >= 9) {
+            this.phoneNumber.text =
+              value.substring(0, 3) +
+              "-" +
+              value.substring(3, 7) +
+              "-" +
+              value.substring(7, 13);
+          }
+        }
+      }
+    },
+
+    // 유효성 검사 로직
+    validateName(value: string): boolean {
+      const result = /^[a-zA-Z]{1,20}$/.test(value);
+      return (this.name.isValid = result);
+    },
+    validateEmail(value: string): boolean {
+      const result = /^[A-Za-z0-9.\-_]+@([A-Za-z0-9-]+\.)+[A-Za-z]{2,6}$/.test(
+        value
+      );
+      return (this.email.isValid = result);
+    },
+    validatePhoneNumber(value: string): boolean {
+      const result = /^\d{2,3}-\d{3,4}-\d{4}$/.test(value);
+      return (this.phoneNumber.isValid = result);
+    },
+
+    // 폼 제출
+    onSubmitInfo(event: HTMLFormElement) {
+      event.preventDefault();
+      this.updateName("");
+      this.updateEmail("");
+      this.updatePhoneNumber("");
+    },
+  },
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+.container {
+  border: 1px solid antiquewhite;
+}
+</style>
