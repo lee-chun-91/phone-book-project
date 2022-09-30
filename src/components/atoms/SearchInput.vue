b0f045bcba08df903bfff787484f64a265bdf71a
Chun-igaw <Chun.lee@igaworks.com>
2022-09-30 19:36:40 +0900

    fix: #8 전화번호 검색 & 정렬 기능 - 검색값 입력 즉시 검색 시작 - 검색값 없을 시 no Reault 출력 - 정렬은 name 기준

--- src/components/atoms/SearchInput.vue
+++ src/components/atoms/SearchInput.vue
@@ -1,9 +1,41 @@
-<template></template>
+<template>
+  <div class="searchInput">
+    <label for="searchInput"></label>
+    <input
+      type="text"
+      id="searchInput"
+      placeholder="검색할 전화번호를 입력해주세요"
+      :value="this.searchText"
+      @input="handleInput"
+    />
+  </div>
+</template>
 
-<script>
-export default {
-  name: "SearchInput.vue",
-};
+<script lang="ts">
+import Vue from "vue";
+
+export default Vue.extend({
+  data() {
+    return {
+      searchText: "",
+    };
+  },
+
+  methods: {
+    handleInput(event: InputEvent) {
+      if (!event.target) {
+        return;
+      }
+      const eventTarget = event.target as HTMLInputElement;
+      this.$emit("input", eventTarget.value);
+    },
+  },
+});
 </script>
 
-<style scoped></style>
+<style scoped>
+#searchInput {
+  height: 30px;
+  width: 200px;
+}
+</style>
--- src/components/molecules/PhoneNumberListTable.vue
+++ src/components/molecules/PhoneNumberListTable.vue
@@ -1,65 +1,104 @@
 <template>
-  <div v-show="tableData.length > 0" class="tableArea">
-    <div class="tableHeader">
-      <h2>전화번호부</h2>
-      <button class="deleteAllButton" @click="deleteAll">전체삭제</button>
+  <div class="phoneNumberListTable">
+    <div class="tableArea">
+      <div class="tableHeader">
+        <h2>전화번호부</h2>
+        <SearchInput @input="search"></SearchInput>
+        <button class="sortByNameButton" @click="sortByName">
+          이름 순 정렬
+        </button>
+        <button class="deleteAllButton" @click="deleteAll">전체삭제</button>
+      </div>
+      <div v-show="dataList.length === 0">No Result</div>
+      <div v-show="dataList.length > 0">
+        <table>
+          <thead style="height: 50px">
+            <tr>
+              <th :key="index" v-for="(value, name, index) in dataList[0]">
+                {{ name }}
+              </th>
+              <th>수정</th>
+              <th>삭제</th>
+            </tr>
+          </thead>
+          <tbody>
+            <tr
+              :key="index"
+              v-for="(info, index) in dataList"
+              :class="coloredClass(index)"
+            >
+              <td :key="index" v-for="(name, item, index) in info">
+                {{ name }}
+              </td>
+              <td>
+                <router-link
+                  :to="{
+                    name: 'UpdatePhoneNumber',
+                    params: { id: dataList.indexOf(info) },
+                  }"
+                >
+                  <button>수정</button></router-link
+                >
+              </td>
+              <td><button @click="phoneNumberDelete(index)">삭제</button></td>
+            </tr>
+          </tbody>
+        </table>
+      </div>
     </div>
-    <table>
-      <thead style="height: 50px">
-      <tr>
-        <th :key="index" v-for="(value, name, index) in tableData[0]">
-          {{ name }}
-        </th>
-        <th>수정</th>
-        <th>삭제</th>
-      </tr>
-      </thead>
-      <tbody>
-        <tr :key="index" v-for="(info, index) in tableData" :class="coloredClass(index)">
-          <td :key="index" v-for="(item, index) in info">{{ item }}</td>
-          <td>
-            <router-link :to="{ name: 'UpdatePhoneNumber', params: { id : index }}">
-              <button @click="phoneNumberEdit(index)">수정</button></router-link>
-
-          </td>
-          <td><button @click="phoneNumberDelete(index)">삭제</button></td>
-        </tr>
-      </tbody>
-    </table>
   </div>
 </template>
 
 <script>
 import Vue from "vue";
 import { MutationTypes } from "@/store/mutations";
+import SearchInput from "@/components/atoms/SearchInput";
 
 export default Vue.extend({
+  components: { SearchInput },
+  data() {
+    return {
+      dataList: [],
+    };
+  },
   methods: {
     coloredClass(rowIndex) {
-      // console.log(rowIndex);
-      // console.log(rowIndex % 2);
       if (rowIndex % 2 === 0) {
         return "colored-row";
       }
       return "";
     },
 
-    phoneNumberEdit(index) {
-      console.log("Edit", index);
+    search(searchText) {
+      this.dataList = this.$store.state.phoneNumberList.filter((item) =>
+        item.phoneNumber.includes(searchText)
+      );
     },
 
+    sortByName() {
+      this.dataList = this.dataList.sort((a, b) => {
+        if (a.name > b.name) {
+          return -1;
+        }
+        if (a.name < b.name) {
+          return 1;
+        }
+        return 0;
+      });
+    },
     phoneNumberDelete(index) {
       this.$store.commit(MutationTypes.DELETE_ITEM, index);
+      this.dataList = this.$store.state.phoneNumberList;
     },
 
     deleteAll() {
       this.$store.commit(MutationTypes.DELETE_ALL);
-    }
-  },
-  computed: {
-    tableData() {
-      return this.$store.state.phoneNumberList;
+      this.dataList = this.$store.state.phoneNumberList;
     },
+  },
+
+  created() {
+    this.dataList = this.$store.state.phoneNumberList;
   },
 });
 </script>
